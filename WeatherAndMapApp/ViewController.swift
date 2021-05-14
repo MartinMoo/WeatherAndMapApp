7d8ef7f5b9a0cfbec4ab0ce4ffedbf4f3c52bd0a
MartinMoo <martin.miklass@gmail.com>
2021-05-14 15:27:37 +0200

    Initial Commit

---
+++ WeatherAndMapApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherAndMapApp
+//
+//  Created by Moo Maa on 14/05/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
